14e013e515995beccc7af2ee1a361f82dd51a546
Siddhu2511 <siddharth.suresh2511@gmail.com>
2023-10-21 16:10:58 +0530

    commit

--- pages/user_profile.py
+++ pages/user_profile.py
@@ -1,4 +1,11 @@
 import flet as ft
 def user_profile(page:ft.Page):
-    page.add(ft.Image(src="/uploads/<some-uploaded-picture.png>"))
+    heading = ft.Text(value="Profile", style=ft.TextThemeStyle.HEADLINE_MEDIUM, color=ft.colors.ORANGE,
+                      font_family="Merienda-Bold")
+    page.add(
+        ft.Column(
+            controls=[heading]
+        ),
+        ft.Image(src="/uploads/<some-uploaded-picture.png>")
+    ),
 ft.app(target=user_profile, assets_dir="assets",upload_dir="assets/uploads")
